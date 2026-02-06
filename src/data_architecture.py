ddbdc54071ee2e0eb9d455ca9ec353f9fa118061
Mojeda01 <marco.oj01@gmail.com>
2026-02-06 21:39:20 +0100

    first iteration of the data structure

--- src/data_architecture.py
+++ src/data_architecture.py
@@ -4,6 +4,81 @@ import pandas as pd
 
 class Data_Architecture:
     def __init__(self):
-        pass
 
-    
+        self.site_schema = {
+            "site_id": "int",
+            "latitude": "float",
+            "longitude": "float",
+            "water_depth_m": "float",
+            "distance_from_shore_km": "float",
+            "avg_current_speed_m_s": "float",
+            "water_temp_c": "float",
+            "salinity_psu": "float",
+            "wave_exposure_index": "float",
+            "regulatory_zone": "category"
+        }
+
+        self.cage_schema = {
+            "cage_id": "int",
+            "site_id": "int",
+            "cage_volume_m3": "float",
+            "stocking_density_kg_m3": "float",
+            "net_mesh_size_mm": "float",
+            "fouling_index": "float",
+            "oxygen_flow_l_min": "float",
+            "maintenance_cycle_days": "int"
+        }
+
+        self.cohort_schema = {
+            "cohort_id": "int",
+            "cage_id": "int",
+            "species": "category",
+            "initial_weight_g": "float",
+            "current_weight_g": "float",
+            "age_days": "int",
+            "survival_rate_pct": "float",
+            "feed_conversion_ratio": "float",
+            "growth_rate_g_day": "float",
+            "disease_status": "category"
+        }
+
+        self.water_quality_schema = {
+            "site_id": "int",
+            "dissolved_oxygen_mg_l": "float",
+            "ammonia_mg_l": "float",
+            "nitrate_mg_l": "float",
+            "turbidity_ntu": "float",
+            "chlorophyll_index": "float"
+        }
+
+        self.operations_schema = {
+            "site_id": "int",
+            "labor_hours_day": "float",
+            "energy_kwh_day": "float",
+            "maintenance_events_month": "int",
+            "treatment_events": "int",
+            "mortality_events": "int"
+        }
+
+        self.financial_schema = {
+            "site_id": "int",
+            "harvest_weight_kg": "float",
+            "market_price_per_kg": "float",
+            "revenue": "float",
+            "cost": "float",
+            "profit_margin": "float"
+        }
+
+    def summary(self):
+        return {
+            "sites": self.site_schema,
+            "cages": self.cage_schema,
+            "cohorts": self.cohort_schema,
+            "water_quality": self.water_quality_schema,
+            "operations": self.operations_schema,
+            "financials": self.financial_schema
+        }
+
+
+x = Data_Architecture()
+print(x.summary())
